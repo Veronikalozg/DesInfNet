95c8106a52026188df8952520e249d8b1748c19d
Veronikalozg <113824431+Veronikalozg@users.noreply.github.com>
2024-12-23 18:18:20 +0300

    Update ClientRepository.java

--- 2laba/ClientRepository.java
+++ 2laba/ClientRepository.java
@@ -16,17 +16,17 @@ class ClientRepository {
     public void setStrategy(ClientStrategy strategy) {
         this.strategy = strategy;
     }
-//Метод для установки стратегии хранения клиентов.
+//b.Метод для установки стратегии хранения клиентов.
     public void saveAllClients() {
         strategy.saveAll(clients);
     }
-//Метод для сохранения всех клиентов, используя стратегию сохранения.
+//a.Метод для сохранения всех клиентов, используя стратегию сохранения.
     public List<Client> readAllClients() {
         return strategy.readAll();
     }
 //Метод для чтения всех клиентов, используя стратегию чтения.
     public Client getById(int clientId) {
-//Метод для получения клиента по его идентификатору (clientId).
+//c.Метод для получения клиента по его идентификатору (clientId).
         for (Client client : clients) {
             if (client.getId() == clientId) {
                 return client;
@@ -36,14 +36,14 @@ class ClientRepository {
         return null;
 //Если клиент не найден, возвращаем null.
     public List<Client> getKNSortList(int k, int n) {
-//Метод для получения подсписка клиентов с пагинацией, где k – номер страницы, а n – количество клиентов на странице.
+//d.Метод для получения подсписка клиентов с пагинацией, где k – номер страницы, а n – количество клиентов на странице.
         int start = (k - 1) * n;
         int end = Math.min(start + n, clients.size());
 //Вычисление начального и конечного индексов для извлечения подсписка клиентов.
         return clients.subList(start, end);
 ///Возвращаем подсписок клиентов на основе вычисленных индексов.
     public void sortByField() {
-//Метод для сортировки списка клиентов по идентификатору.
+//e.Метод для сортировки списка клиентов по идентификатору.
         clients.sort(Comparator.comparing(Client::getId));
 //Использование компаратора для сортировки клиентов по их идентификатору.
     protected boolean isUnique(String phone) {
@@ -57,7 +57,7 @@ class ClientRepository {
         return true;
 //Если номер телефона уникален, возвращаем true.
     public void addClient(Client client) throws Exception {
-//Метод для добавления нового клиента.
+//f.Метод для добавления нового клиента.
         int newId = clients.stream().mapToInt(Client::getId).max().orElse(0) + 1;
 //Определение нового идентификатора для клиента как максимального идентификатора в списке клиентов плюс один. Если список пуст, идентификатор будет равен 1.
         if (!isUnique(client.getPhone())) {
@@ -69,7 +69,7 @@ class ClientRepository {
         clients.add(newClient);
 //Добавление созданного клиента в список.
     public boolean replaceById(int clientId, Client newClient) throws Exception {
-//Метод для замены информации о клиенте по его идентификатору.
+//g.Метод для замены информации о клиенте по его идентификатору.
         for (int i = 0; i < clients.size(); i++) {
             if (clients.get(i).getId() == clientId) {
 //Перебор списка клиентов для поиска клиента с указанным идентификатором.
@@ -85,13 +85,13 @@ class ClientRepository {
         return false;
 //Если клиент с указанным идентификатором не найден, возвращаем false.
     public void deleteById(int clientId) {
-//Метод для удаления клиента по его идентификатору.
+//h.Метод для удаления клиента по его идентификатору.
         clients = clients.stream()
                 .filter(customer -> customer.getId() != clientId)
                 .collect(Collectors.toList());
 //Использование стрима для фильтрации списка клиентов: оставляем только тех, у кого идентификатор не совпадает с удаляемым. Результат сохраняем обратно в список клиентов.
     public int getCount() {
-//Метод для получения количества клиентов.
+//i.Метод для получения количества клиентов.
         return clients.size();
 //Возвращение размера списка клиентов, то есть их количества.;
     }
