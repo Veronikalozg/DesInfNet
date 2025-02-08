e695d7cac4fb15f240ee2eaeaea9ee6a8d23e137
Veronikalozg <113824431+Veronikalozg@users.noreply.github.com>
2025-02-08 10:50:10 +0300

    Update ClientRepositoryAdapter.java

--- 2laba/ClientRepositoryAdapter.java
+++ 2laba/ClientRepositoryAdapter.java
@@ -1,45 +1,53 @@
-class ClientRepositoryAdapter {
-//Определение класса ClientRepositoryAdapter, который будет служить адаптером для работы с ClientRepository.
+import java.util.ArrayList;
+import java.util.List;
+
+class ClientRepositoryAdapter implements IClientRepository {
+    private List<ClientObserver> observers = new ArrayList<>();
     private ClientRepository clientRepository;
-//Приватное поле для хранения экземпляра ClientRepository, который будет использоваться для выполнения операций с клиентами.
+
     public ClientRepositoryAdapter(String filename, ClientStrategy strategy) {
-//Конструктор класса, который принимает имя файла и стратегию хранения клиентов.
         this.clientRepository = new ClientRepository(filename, strategy);
-//Инициализация поля clientRepository, создавая новый объект ClientRepository с переданными параметрами.
+    }
+
     public Client getById(int clientId) {
-//Метод для получения клиента по его идентификатору (clientId).
         return  clientRepository.getById(clientId);
-//Вызов метода getById на объекте clientRepository и возврат результата.
+    }
+
     public List<Client> getKNSortList(int k, int n) {
-//Метод для получения подсписка клиентов с пагинацией.
         return clientRepository.getKNSortList(k, n);
-//Вызов метода getKNSortList на объекте clientRepository и возврат результата.
+    }
+
     public void addClient(Client client) throws Exception {
-//Метод для добавления нового клиента.
         clientRepository.addClient(client);
-//Вызов метода addClient на объекте clientRepository, чтобы добавить клиента.
         clientRepository.saveAllClients();
-//Сохранение всех клиентов после добавления нового.
+        notifyObservers();
+    }
+
     public boolean replaceById(int clientId, Client newClient) throws Exception {
-//Метод для замены информации о клиенте по его идентификатору.
         boolean flag = clientRepository.replaceById(clientId, newClient);
-//Вызов метода replaceById на объекте clientRepository и сохранение результата в переменной flag.
         if(flag) {
             clientRepository.saveAllClients();
             return true;
         }
-//Если замена прошла успешно (flag равно true), сохраняем всех клиентов и возвращаем true.
         return false;
-//Если замена не прошла успешно, возвращаем false.
-    public void deleteById(int clientId) {
-//Метод для удаления клиента по его идентификатору.
-        clientRepository.deleteById(clientId);
-//Вызов метода deleteById на объекте clientRepository, чтобы удалить клиента.
-        clientRepository.saveAllClients();
-//Сохранение всех клиентов после удаления.
-    public int getCount() {
-//Метод для получения количества клиентов.
-        return clientRepository.getCount();
-//Вызов метода getCount на объекте clientRepository и возвращение результата.;
     }
-}
+
+    public boolean updateClient(int clientId, Client newClient) throws Exception {
+        boolean flag = clientRepository.updateClient(clientId, newClient);
+        if(flag) {
+            clientRepository.saveAllClients();
+            notifyObservers();
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteById(int clientId) {
+        clientRepository.deleteById(clientId);
+        clientRepository.saveAllClients();
+        notifyObservers();
+    }
+
+    public int getCount() {
+        return clientRepository.getCount();
+    }
